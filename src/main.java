ea8d7c848603f7b872d3fe44e9240cf770e4e57c
Jesse <jesseberliner@gmail.com>
2017-07-28 10:17:28 -0400

    Basic requirements fulfilled

--- src/main.java
+++ src/main.java
@@ -6,36 +6,75 @@ public class main {
 		// TODO Auto-generated method stub
 		
 		//Instantiations
-		ArrayList <Product> list = new ArrayList();
+		Product product;// = new Product();
+		ArrayList <Product> productList = new ArrayList();
 		Scanner scan = new Scanner(System.in);
-		Product test = new Product();
-		
+				
 		//variables
-		double total;
-		int taxRate;
-		double taxAmt;
+		boolean isDone=false;
+		double subtotal=0;
+		double total=0;
+		int taxRateInt=0;
+		double taxRate=0;
+		double taxAmt=0.0;
+		String choice="y";
+		
+		//get tax rate
+		System.out.println("What is the tax rate? Please enter an integer.");
+		taxRate=scan.nextDouble();
 		
 		
-		System.out.println("What is the tax rate?");
-		taxRate=scan.nextInt();
-		
-		//put this into a loop to gather more than one item
-		System.out.println("What's the first item price?");
-		test.setPrice(scan.nextDouble());
-		System.out.println("What's the first item description?");
-		test.setDescr(scan.next());
-		//add Product object to array
 		
 		
-		System.out.printf("Your price: %f and description: %s",test.getPrice(), test.getDescr());
+		while(!isDone) //start loop
+		{
+			product = new Product();
+			//get item price
+			System.out.println("What's the item price?");
+			product.setPrice(scan.nextDouble());
+			//get item description
+			System.out.println("What's the item description?");
+			product.setDescr(scan.next());
+			//add Product object to array
+			productList.add(product);
+			//prompt if user wants to add another item
+			System.out.println("Do you want to add another item? 'y/n'");
+			choice=scan.next();
+			//if not, exit loop
+			if(choice.equalsIgnoreCase("n"))
+			{
+				isDone=true;
+			}
+			//if they didn't say y(es) or n(o), error message, reprompt
+			/*TODO 
+			 * Currently this only reprompts, then exits the loop
+			 * Need to handle error and accept reprompt
+			 */
+			else if(!choice.equalsIgnoreCase("y"))
+			{
+				System.out.println("Incorrect choice.  Please type 'y' or 'n'");
+				choice=scan.nextLine();
+				isDone=true;
+			}
+		}
 		
-		//when out of loop
-		//enhanced For to loop through each product price and add it to total
-		//also print price
+		//iterate through ArrayList and 1)add price to subtotal and 2)print the price
+		for(int counter=0; counter<productList.size(); counter++) //maybe can be done with forEach and a lambda exp?
+		{
+			//product= new Product();
+			subtotal+=productList.get(counter).getPrice();
+			System.out.printf("The price of this item is $%.2f\n",productList.get(counter).getPrice());
+		}
+
+		taxAmt=(taxRate/100)*subtotal;//calculate tax amount
+		total=subtotal+taxAmt;//calculate total
 		
-		//calculate tax amount (taxRate/100)*total
-		//print tax due
-		//print total
+		System.out.printf("The total tax is: $%.2f\n",taxAmt); //print tax amount
+		System.out.printf("Your total is: $%.2f\n",total); //print total
+		
+		
+		
+		
 		
 	}
 
